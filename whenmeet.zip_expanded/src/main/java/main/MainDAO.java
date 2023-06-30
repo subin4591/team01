64960e4af3daae4901ecac65692ba5a9ada15ef2
Dohyun Kwon <gerrerd@naver.com>
2023-06-30 15:45:11 +0900

    수정

---
+++ whenmeet.zip_expanded/src/main/java/main/MainDAO.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface MainDAO {
+
+	public List<String> myApplication(String user_id);
+	public List<String> myWrite(String user_id);
+	public List<String> myGroup(String user_id);
+	public List<String> rankList();
+	
+}
