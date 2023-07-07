b0d75fc5adf677efcfbc0c2c25868e340c22a5b8
rainvi <g0119@naver.com>
2023-07-07 14:49:44 +0900

    통합(db 없으면 오류날 수 있음)

--- whenmeet/src/main/java/schedule/scheduleService.java
+++ whenmeet/src/main/java/schedule/scheduleService.java
@@ -1,5 +1,6 @@
 package schedule;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +19,24 @@ public class scheduleService {
 	public List<UserDTO> selectUser() throws Exception {
 		return scheduleMapper.selectUser();
 	}
-
-	public List<GroupDTO> selectGroup() throws Exception{
-		return scheduleMapper.selectGroup();
+	public UserDTO selectUserOne(String user_id) throws Exception {
+		return scheduleMapper.selectUserOne(user_id);
 	}
 	
+	public List<GroupDTO> selectGroup() throws Exception{
+		return scheduleMapper.selectGroup();
+	}	
 	public GroupDTO selectGroupOne(String group_id) throws Exception {
 		return scheduleMapper.selectGroupOne(group_id);
 	}
 	
 	public List<GroupUserDTO> selectGroupUser() throws Exception{
 		return scheduleMapper.selectGroupUser();
+	}
+	public GroupUserDTO selectGroupUserOne(HashMap<String, String> map) throws Exception{
+		return scheduleMapper.selectGroupUserOne(map);
+	}
+	public List<GroupUserDTO> selectGroupUsers(String group_id) throws Exception{
+		return scheduleMapper.selectGroupUsers(group_id);
 	}
 }
